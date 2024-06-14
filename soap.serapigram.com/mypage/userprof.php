d126eec26b62238a53322b3f2ae886940f52fcc7
YF0414 <160362580+YF0414@users.noreply.github.com>
2024-06-14 10:34:02 +0900

    Update userprof.php

--- soap.serapigram.com/mypage/userprof.php
+++ soap.serapigram.com/mypage/userprof.php
@@ -42,7 +42,7 @@ include $inc_path .'/inc/head.php';
 
 <div class="button-box">
   <a href="/mypage" class="mat-button">マイページトップ</a>
-  <a href="therapist.php" class="mat-button">セラピスト</a>
+  <a href="therapist.php" class="mat-button">女性</a>
   <a href="likeshop.php" class="mat-button">ショップ</a>
   <a href="userprof.php" class="mat-button pushpush">ユーザー情報</a>
 </div>
